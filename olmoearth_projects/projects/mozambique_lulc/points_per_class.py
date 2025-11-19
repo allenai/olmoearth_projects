367f31c57225817fc6885452564600feb8a55d01
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2025-11-19 09:37:03 +0000

    Cleanup points_per_class script

--- olmoearth_projects/projects/mozambique_lulc/points_per_class.py
+++ olmoearth_projects/projects/mozambique_lulc/points_per_class.py
@@ -2,61 +2,8 @@
 
 import argparse
 
-import numpy as np
 from rslearn.dataset.dataset import Dataset
-from rslearn.dataset.window import Window
-from rslearn.utils.raster_format import GeotiffRasterFormat
-from rslearn.utils.vector_format import GeojsonVectorFormat
 from upath import UPath
-
-LULC_CLASS_NAMES = [
-    "invalid",
-    "Water",
-    "Bare Ground",
-    "Rangeland",
-    "Flooded Vegetation",
-    "Trees",
-    "Cropland",
-    "Buildings",
-]
-CROPTYPE_CLASS_NAMES = [
-    "invalid",
-    "corn",
-    "cassava",
-    "rice",
-    "sesame",
-    "beans",
-    "millet",
-    "sorghum",
-]
-PROPERTY_NAME = "category"
-BAND_NAME = "label"
-
-
-def create_label_raster(window: Window) -> None:
-    """Create label raster for the given window."""
-    label_dir = window.get_layer_dir("label")
-    features = GeojsonVectorFormat().decode_vector(
-        label_dir, window.projection, window.bounds
-    )
-    class_name = features[0].properties[PROPERTY_NAME]
-    try:
-        class_id = LULC_CLASS_NAMES.index(class_name)
-    except ValueError:
-        class_id = CROPTYPE_CLASS_NAMES.index(class_name)
-
-    # Draw the class_id in the middle 1x1 of the raster.
-    raster = np.zeros(
-        (1, window.bounds[3] - window.bounds[1], window.bounds[2] - window.bounds[0]),
-        dtype=np.uint8,
-    )
-    raster[:, raster.shape[1] // 2, raster.shape[2] // 2] = class_id
-    raster_dir = window.get_raster_dir("label_raster", [BAND_NAME])
-    GeotiffRasterFormat().encode_raster(
-        raster_dir, window.projection, window.bounds, raster
-    )
-    window.mark_layer_completed("label_raster")
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
