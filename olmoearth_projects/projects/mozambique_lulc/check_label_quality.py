3a9b6e6b6aea90015111e39ef415ef298b221962
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2025-12-05 08:06:46 +0000

    check label quality on an rslearn project

---
+++ olmoearth_projects/projects/mozambique_lulc/check_label_quality.py
@@ -0,0 +1,51 @@
+"""Check the label quality of the dataset."""
+
+import argparse
+
+import geopandas as gpd
+from rslearn.dataset.dataset import Dataset
+from shapely.geometry import box
+from upath import UPath
+
+from olmoearth_projects.utils.label_quality import check_label_quality
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--ds_path",
+        type=str,
+        required=True,
+        help="Path to the dataset",
+    )
+    parser.add_argument(
+        "--split",
+        type=str,
+        default="train",
+        help="Split to assess. Use 'all' to assess all splits",
+    )
+    parser.add_argument("--crop_type", action="store_true", default=False)
+
+    args = parser.parse_args()
+
+    if args.split == "all":
+        splits_to_keep = ["train", "val", "test"]
+    else:
+        splits_to_keep = [args.split]
+
+    dataset = Dataset(UPath(args.ds_path))
+    if args.crop_type:
+        groups = ["crop_type"]
+    else:
+        groups = ["gaza", "manica", "zambezia"]
+    windows = dataset.load_windows(
+        workers=args.workers, show_progress=True, groups=groups
+    )
+    labels, geometry = [], []
+    for window in windows:
+        split = window.options["split"]
+        if split in splits_to_keep:
+            labels.append(window.options["category"])
+            geometry.append(box(*window.bounds))
+
+    df = gpd.GeoDataFrame({"label": labels, "geometry": geometry})
+    check_label_quality(df)
