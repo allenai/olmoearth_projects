5a449d8a6c4b5b4d5b8e5b31c810eb138bc9c788
Favyen Bastani <favyenb@allenai.org>
2025-12-19 12:32:17 -0800

    Forest loss driver prediction pipeline on OlmoEarth platform.

---
+++ olmoearth_projects/projects/forest_loss_driver/deploy/sentinel2.py
@@ -0,0 +1,98 @@
+"""Utilities for identifying Sentinel-2 assets to show for a forest loss event."""
+
+import functools
+import multiprocessing
+from datetime import datetime, timedelta
+from typing import Any
+
+import tqdm
+from rslearn.config import QueryConfig, SpaceMode
+from rslearn.data_sources.data_source import Item
+from rslearn.data_sources.planetary_computer import Sentinel2
+from rslearn.dataset.manage import retry
+from rslearn.utils.feature import Feature
+from rslearn.utils.geometry import STGeometry
+
+# Duration and offset modifications to make to the forest loss event timestamp when
+# looking for assets for visualization.
+DURATION = timedelta(days=180)
+PRE_OFFSET = timedelta(days=-300)
+POST_OFFSET = timedelta(days=7)
+
+
+@functools.cache
+def _get_data_source() -> Sentinel2:
+    """Get cached data source for identifying assets."""
+    return Sentinel2(sort_by="eo:cloud_cover")
+
+
+def _get_assets_for_feat(feat: Feature) -> tuple[list[dict], list[dict]]:
+    """Get the Sentinel-2 assets for a feature.
+
+    Args:
+        feat: the feature to identify assets for.
+
+    Returns: a tuple (pre_assets, post_assets) containing lists of Planetary Computer
+        asset URLs for pre-event Sentinel-2 TCI images and post-event images.
+    """
+    data_source = _get_data_source()
+
+    def get_assets_for_time_offset(
+        offset: timedelta, duration: timedelta
+    ) -> list[dict[str, Any]]:
+        """Get assets after applying a time offset on the feature geometry."""
+        ts = datetime.fromisoformat(feat.properties["oe_start_time"])
+        geometry = STGeometry(
+            feat.geometry.projection,
+            feat.geometry.shp,
+            (ts + offset, ts + offset + duration),
+        )
+        query_config = QueryConfig(
+            space_mode=SpaceMode.CONTAINS,
+            max_matches=3,
+        )
+        # Run request with retries since Planetary Computer often has transient errors.
+        item_groups: list[list[Item]] = retry(
+            fn=lambda: data_source.get_items([geometry], query_config)[0],
+            retry_max_attempts=10,
+            retry_backoff=timedelta(seconds=5),
+        )
+
+        assets: list[dict] = []
+        for item_group in item_groups:
+            assert len(item_group) == 1
+            item = item_group[0]
+            assets.append(
+                dict(
+                    url=item.asset_urls["visual"],
+                    ts=item.geometry.time_range[0].isoformat(),
+                )
+            )
+
+        return assets
+
+    pre_assets = get_assets_for_time_offset(PRE_OFFSET, DURATION)
+    post_assets = get_assets_for_time_offset(POST_OFFSET, DURATION)
+    return (pre_assets, post_assets)
+
+
+def get_sentinel2_assets(features: list[Feature], workers: int) -> None:
+    """Identify suitable pre and post Sentinel-2 assets for each feature.
+
+    The Planetary Computer asset URLs are added as properties to the feature. The
+    assets are used for visualization in the website.
+
+    Args:
+        features: the forest loss event features. Their properties will be updated.
+        workers: number of worker processes to use.
+    """
+    p = multiprocessing.Pool(workers)
+    outputs = tqdm.tqdm(
+        p.imap(_get_assets_for_feat, features),
+        desc="Identify pre/post Sentinel-2 assets",
+        total=len(features),
+    )
+    for feat, (pre_assets, post_assets) in zip(features, outputs):
+        feat.properties["pre_assets"] = pre_assets
+        feat.properties["post_assets"] = post_assets
+    p.close()
---
+++ olmoearth_run_data/forest_loss_driver/deploy.Dockerfile
@@ -0,0 +1,23 @@
+FROM pytorch/pytorch:2.7.0-cuda12.8-cudnn9-runtime@sha256:7db0e1bf4b1ac274ea09cf6358ab516f8a5c7d3d0e02311bed445f7e236a5d80
+
+RUN apt-get update && apt-get install -y git --no-install-recommends && rm -rf /var/lib/apt/lists/*
+
+COPY --from=ghcr.io/astral-sh/uv:latest /uv /usr/local/bin/uv
+
+# Install tippecanoe, it is used by the forest loss driver inference pipeline for
+# making the slippy tiles that appear in the web app.
+RUN apt install -y build-essential libsqlite3-dev zlib1g-dev
+RUN git clone https://github.com/mapbox/tippecanoe /opt/tippecanoe
+WORKDIR /opt/tippecanoe
+RUN make -j
+RUN make install
+
+WORKDIR /olmoearth_projects
+
+COPY pyproject.toml /olmoearth_projects/pyproject.toml
+COPY uv.lock /rslearn/uv.lock
+RUN uv sync --all-extras --no-install-project
+
+ENV PATH="/olmoearth_projects/.venv/bin:$PATH"
+COPY ./ /olmoearth_projects
+RUN uv sync --all-extras --locked
